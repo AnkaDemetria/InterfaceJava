97d4886453077cb665b98c3764bb8f4398797308
AnkaDemetria <116574892+AnkaDemetria@users.noreply.github.com>
2023-06-17 09:07:21 +0200

    commit readme

--- com/bank/BankAccount.java
+++ com/bank/BankAccount.java
@@ -3,11 +3,12 @@ package com.bank;
 import com.actions.Writable;
 import com.actions.Readable;
 
-public class BankAccount extends Test implements CardBankable, Writable, Readable {
+public abstract class BankAccount extends Test implements CardBankable, Writable, Readable {
     public BankAccount(String money) {
         super(money);
     }
-    // protected String money;
+
+    protected String money;
 
     public void getMoney(int amount) {
         System.out.println("vous avez retiré" + amount + "euros");
